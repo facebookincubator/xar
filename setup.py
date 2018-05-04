56e674d8f9c1f5aea22dc6cd4b0adbd1cd22610c
Cooper Ry Lees <me@cooperlees.com>
2018-05-03 22:27:31 -0700

    Polish up setup.py ready for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
+import os
 from setuptools import setup
 
 from xar.commands.bdist_xar import bdist_xar
 
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_long_description():
+    with open(os.path.join(CURRENT_DIR, "README.md"), "r") as ld_file:
+        return ld_file.read()
+
+
 setup(
     name='xar',
-    version='0.1',
+    version='0.69',
     description="The XAR packaging toolchain.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Chip Turner",
+    author_email="chip@fb.com",
     url='https://github.com/facebook/xar',
+    license="BSD",
     packages=[
         'xar',
         'xar.commands',
@@ -20,9 +30,19 @@ setup(
         # Version 34.1 fixes a bug in the dependency resolution. If this is
         # causing an problem for you, please open an issue, and we can evaluate
         # a workaround. (grep setuptools>=34.1 to see issue)
-        # https://github.com/pypa/setuptools/commit/8c1f489f09434f42080397367b6491e75f64d838  # noqa: B950
+        # https://github.com/pypa/setuptools/commit/8c1f489f09434f42080397367b6491e75f64d838  # noqa: E501
         "setuptools>=34.1",
         "wheel",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Framework :: Setuptools Plugin",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
     ],
     entry_points={
         'distutils.commands': [
